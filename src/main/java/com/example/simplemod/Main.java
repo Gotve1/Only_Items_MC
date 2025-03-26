e099430557087a64ccb2339b100a8cc0680346a0
Sunrise <akselvolkov40@gmail.com>
2025-03-26 14:46:46 +0500

    awghsjkdl

---
+++ src/main/java/com/example/simplemod/Main.java
@@ -0,0 +1,36 @@
+package com.example.simplemod;
+
+import com.example.simplemod.proxy.CommonProxy;
+import com.example.simplemod.util.Reference;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.SidedProxy;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = Reference.MOD_ID,
+        name = Reference.NAME,
+        version = Reference.VERSION)
+public class Main {
+
+    @Mod.Instance
+    public static Main instance;
+
+    @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS,
+                serverSide = Reference.COMMON_PROXY_CLASS)
+    public static CommonProxy proxy;
+
+    @Mod.EventHandler
+    public static void PreInit(FMLPreInitializationEvent event){
+
+    }
+    @Mod.EventHandler
+    public static void Init(FMLInitializationEvent event){
+
+    }
+    @Mod.EventHandler
+    public static void PostInit(FMLPostInitializationEvent event){
+
+    }
+
+}
---
+++ src/main/java/com/example/simplemod/util/Reference.java
@@ -0,0 +1,12 @@
+package com.example.simplemod.util;
+
+public class Reference {
+
+    public static final String MOD_ID = "simplemod";
+    public static final String NAME = "Simple Mod";
+    public static final String VERSION = "1.0.0";
+    public static final String ACCEPTED_VERSIONS = "[1.12.2]";
+    public static final String CLIENT_PROXY_CLASS = "com.example.simplemod.proxy.ClientProxy";
+    public static final String COMMON_PROXY_CLASS = "com.example.simplemod.proxy.CommonProxy";
+
+}
